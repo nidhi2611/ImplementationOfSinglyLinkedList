8acfd942e20330e0c3db158cb3971ccea13925e1
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-02-24 23:39:31 +0530

    constructor defined

--- src/singlylinkedlist/SinglyLinkedList.java
+++ src/singlylinkedlist/SinglyLinkedList.java
@@ -5,4 +5,12 @@ import node.Node;
 public class SinglyLinkedList {
     Node head;
 
+    public SinglyLinkedList() {
+        head = null;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
 }
